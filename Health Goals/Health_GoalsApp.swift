1dcbc500c36264a4518fab1bcd89092297a1ed23
Jaba Kochashvili <43106084+kochashvili@users.noreply.github.com>
2024-02-17 23:08:32 +0400

    Initial Commit

---
+++ Health Goals/Health_GoalsApp.swift
@@ -0,0 +1,32 @@
+//
+//  Health_GoalsApp.swift
+//  Health Goals
+//
+//  Created by Jaba Kochashvili on 2/17/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Health_GoalsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
